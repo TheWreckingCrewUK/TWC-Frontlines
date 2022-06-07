1c4169633468048cfa8923743ca40739377be334
rwalker959 <rwalker959@gmail.com>
2022-06-07 13:12:52 +0100

    Start of Frontlines Ruha

---
+++ Frontlines.ruha/Config/Config_Blufor.hpp
@@ -0,0 +1,284 @@
+/*
+	Faction: Bundeswehr (CUP)
+	Author: Ross
+	Requires: CUP, 3CB
+*/
+class Blufor_Setup {
+	AW_fobBuilding = "Land_Cargo_HQ_V1_F";
+
+	AW_bluforCrates[] = { //the order links to the variable containing all resources
+		"CargoNet_01_box_F", //supplies
+		"CargoNet_01_barrels_F", //fuel
+		"B_CargoNet_01_ammo_F" //ammo
+	};
+
+	AW_fobBoxes[] = { //vehicles that FOBs can be created from
+		"B_Slingload_01_Cargo_F",
+		"B_Truck_01_box_F"
+	};
+
+	AW_mobileRespawns[] = { //vehicles that act as mobile respawns
+		"UK3CB_BAF_MAN_HX58_Container_Green",
+		"UK3CB_BAF_MAN_HX58_Cargo_Green_B"
+    };
+    
+	AW_medicalBuildings[] = { //buildings to add heal actions onto
+		"RU_WarfareBFieldhHospital"
+	};
+
+	//Building menu options below, format {"Classname",{Supplies,Fuel,Ammo},"Any extra conditions"}
+	AW_bluforLightVehicles[] = {
+		{"B_Quadbike_01_F", {50,25,0}, ""},
+
+		{"UK3CB_BAF_LandRover_Soft_Green_A", {60,25,0}, ""},						//Land Rover
+		{"UK3CB_BAF_LandRover_Soft_FFR_Green_A", {60,25,0}, ""},                   
+		{"UK3CB_BAF_LandRover_Hard_Green_A", {70,25,0}, ""},                      	
+		{"UK3CB_BAF_LandRover_Hard_FFR_Green_A", {70,25,0}, ""},                   
+		{"UK3CB_BAF_LandRover_Snatch_Green_A", {80,25,0}, ""},                     
+		{"UK3CB_BAF_LandRover_Snatch_FFR_Green_A", {80,25,0}, ""},                 
+		
+		{"UK3CB_BAF_LandRover_WMIK_GPMG_Green_A", {75,25,50}, ""},					//WMIK
+		{"UK3CB_BAF_LandRover_WMIK_GPMG_FFR_Green_A", {75,25,50}, ""},             
+		{"UK3CB_BAF_LandRover_WMIK_HMG_Green_A", {80,25,50}, ""},                  
+		{"UK3CB_BAF_LandRover_WMIK_HMG_FFR_Green_A", {80,25,50}, ""},              
+		{"UK3CB_BAF_LandRover_WMIK_GMG_Green_A", {80,25,75}, ""},                  
+		{"UK3CB_BAF_LandRover_WMIK_GMG_FFR_Green_A", {80,25,75}, ""},             
+		{"UK3CB_BAF_LandRover_WMIK_Milan_Green_A", {100,25,100}, ""},              
+		{"UK3CB_BAF_LandRover_WMIK_Milan_FFR_Green_A", {100,25,100}, ""},
+		
+		{"CUP_B_Dingo_GL_GER_Wdl", {200,50,200}, ""},								//Dingo
+		{"CUP_B_Dingo_GER_Wdl", {200,50,200}, ""},			
+		
+		{"CUP_B_FENNEK_GER_Wdl", {100,25,100}, ""},									//Fennek
+
+		{"UK3CB_BAF_MAN_HX60_Cargo_Green_A", {100,100,0}, ""},                      // MAN 4x4
+		{"UK3CB_BAF_MAN_HX60_Transport_Green", {100,100,0}, ""},
+		
+		{"CUP_B_MTVR_Ammo_BAF_WOOD", {100,100,0}, ""},								// MTVR Ammo Truck
+		
+		{"UK3CB_BAF_MAN_HX58_Cargo_Green_A", {120,100,0}, ""},                      // MAN 6x6
+		{"UK3CB_BAF_MAN_HX58_Transport_Green", {120,100,0}, ""}
+		
+	};
+
+	AW_bluforHeavyVehicles[] = {
+		{"CUP_B_M113A3_GER", {200,150,200}, ""}, //
+
+		{"CUP_B_Boxer_GMG_GER_WDL", {300,200,200}, ""},
+		{"CUP_B_Boxer_HMG_GER_WDL", {300,200,200}, ""},
+
+		{"CUP_B_Leopard2A6_GER", {600,400,600}, ""}
+	};
+
+	AW_bluforAirVehicles[] = {
+		{"B_UAV_01_F", {50,100,0}, ""},
+		{"B_UAV_06_F", {75,100,0}, ""},
+		{"B_UAV_02_dynamicLoadout_F", {300,200,200}, "(nearestObjects [player,['B_Radar_System_01_F'],150]) isNotEqualTo [] && {AW_civRep > 50 && {call AW_fnc_getMissionProgress > 0.35}}"},
+		{"B_UAV_05_F", {300,300,250}, "(nearestObjects [player,['B_Radar_System_01_F'],150]) isNotEqualTo [] && {AW_civRep > 50 && {call AW_fnc_getMissionProgress > 0.35}}"},
+
+		{"B_Heli_Light_01_F", {200,150,0}, "(nearestObjects [player,['B_Radar_System_01_F'],150]) isNotEqualTo [] && {count AW_currentHelicopters < AW_maxHelicopterCount}"},                      
+		{"B_Heli_Light_01_dynamicLoadout_F", {250,150,150}, "(nearestObjects [player,['B_Radar_System_01_F'],150]) isNotEqualTo [] && {count AW_currentHelicopters < AW_maxHelicopterCount}"},
+		
+		{"B_Heli_Transport_03_F", {300,200,250}, "(nearestObjects [player,['B_Radar_System_01_F'],150]) isNotEqualTo [] && {count AW_currentHelicopters < AW_maxHelicopterCount}"},							
+		{"B_Heli_Transport_01_F", {200,200,100}, "(nearestObjects [player,['B_Radar_System_01_F'],150]) isNotEqualTo [] && {count AW_currentHelicopters < AW_maxHelicopterCount}"},
+		{"B_CTRG_Heli_Transport_01_sand_F", {200,200,100}, "(nearestObjects [player,['B_Radar_System_01_F'],150]) isNotEqualTo [] && {count AW_currentHelicopters < AW_maxHelicopterCount}"},
+		{"B_CTRG_Heli_Transport_01_tropic_F", {200,200,100}, "(nearestObjects [player,['B_Radar_System_01_F'],150]) isNotEqualTo [] && {count AW_currentHelicopters < AW_maxHelicopterCount}"},   
+
+		{"B_T_VTOL_01_infantry_F", {600,300,0}, "(nearestObjects [player,['B_Radar_System_01_F'],150]) isNotEqualTo [] && {count AW_currentPlanes < AW_maxPlaneCount}"},
+        {"B_T_VTOL_01_vehicle_F", {600,300,0}, "(nearestObjects [player,['B_Radar_System_01_F'],150]) isNotEqualTo [] && {count AW_currentPlanes < AW_maxPlaneCount}"},
+        {"B_T_VTOL_01_armed_F", {600,300,700}, "(nearestObjects [player,['B_Radar_System_01_F'],150]) isNotEqualTo [] && {count AW_currentPlanes < AW_maxPlaneCount}"}, 
+		
+		{"B_Plane_Fighter_01_F", {900,800,800}, "(nearestObjects [player,['B_Radar_System_01_F'],150]) isNotEqualTo [] && {count AW_currentPlanes < AW_maxPlaneCount && {AW_civRep > 30 && {call AW_fnc_getMissionProgress > 0.4}}}"},
+		{"B_Plane_CAS_01_dynamicLoadout_F", {800,800,900}, "(nearestObjects [player,['B_Radar_System_01_F'],150]) isNotEqualTo [] && {count AW_currentPlanes < AW_maxPlaneCount && {AW_civRep > 30 && {call AW_fnc_getMissionProgress > 0.35}}}"}								
+	};
+
+	AW_bluforNavalVehicles[] = {};
+
+	AW_bluforDefences[] = {
+		{"B_SAM_System_03_F", {400,0,500}, ""},                                       
+		{"B_AAA_System_01_F", {400,0,500}, ""},                                       
+		{"B_static_AA_F", {100,0,50}, ""}                                       
+	};
+
+	AW_bluforBuildings[] = {
+		{"RU_WarfareBFieldhHospital", {30,0,0}, ""},
+
+		{"FlagCarrierGermany_EP1", {5,0,0}, ""},
+
+		{"Land_CamoNet_NATO", {5,0,0}, ""},
+		{"Land_CamoNetVar_NATO", {5,0,0}, ""},
+		{"Land_CamoNetB_NATO", {10,0,0}, ""},
+
+		{"Land_PortableLight_single_F", {10,0,0}, ""},
+		{"Land_PortableLight_double_F", {10,0,0}, ""},
+		{"PortableHelipadLight_01_blue_F", {10,0,0}, ""},
+		{"PortableHelipadLight_01_green_F", {10,0,0}, ""},
+		{"PortableHelipadLight_01_red_F", {10,0,0}, ""},
+		{"Land_Camping_Light_F", {10,0,0}, ""},
+		{"Land_TentLamp_01_standing_F", {10,0,0}, ""},
+		{"Land_TentLamp_01_standing_red_F", {10,0,0}, ""},
+		{"Land_TentLamp_01_suspended_F", {10,0,0}, ""},
+		{"Land_TentLamp_01_suspended_red_F", {10,0,0}, ""},
+
+		{"Land_BagFence_round_F", {10,0,0}, ""},
+		{"Land_BagFence_long_F", {10,0,0}, ""},
+		{"Land_BagFence_corner_F", {10,0,0}, ""},
+		{"Land_BagFence_end_F", {10,0,0}, ""},
+		{"Land_fort_bagfence_long", {10,0,0}, ""},
+		{"Land_fort_bagfence_corner", {10,0,0}, ""},
+		{"Land_fort_bagfence_round", {10,0,0}, ""},
+		{"Land_fort_rampart", {25,0,0}, ""},
+		{"Land_fort_artillery_nest", {50,0,0}, ""},
+		{"Land_BagBunker_small_F", {15,0,0}, ""},
+		{"Land_BagBunker_large_F", {15,0,0}, ""},
+		{"Land_HBarrierTower_F", {75,0,0}, ""},
+
+		{"Land_HBarrier_1_F", {5,0,0}, ""},
+		{"Land_HBarrier_3_F", {15,0,0}, ""},
+		{"Land_HBarrier_5_F", {20,0,0}, ""},
+		{"Land_HBarrier_Big_F", {50,0,0}, ""},
+		{"Land_HBarrierWall4_F", {20,0,0}, ""},
+		{"Land_HBarrierWall6_F", {25,0,0}, ""},
+		{"Land_HBarrierWall_corner_F", {50,0,0}, ""},
+		{"Land_HBarrierWall_corridor_F", {25,0,0}, ""},
+		{"Land_HBarrierTower_F", {100,0,0}, ""},
+
+		{"BlockConcrete_F", {30,0,0}, ""},
+		{"Land_RampConcrete_F", {30,0,0}, ""},
+		{"Land_RampConcreteHigh_F", {30,0,0}, ""},
+
+		{"Land_Razorwire_F", {15,0,0}, ""},
+		{"Land_Mil_WiredFence_F", {25,0,0}, ""},
+		{"Land_ConcreteHedgehog_01_F", {15,0,0}, ""},
+		{"Land_CzechHedgehog_01_old_F", {15,0,0}, ""},
+
+		{"Land_CampingChair_V1_F", {5,0,0}, ""},
+		{"Land_CampingChair_V2_F", {5,0,0}, ""},
+		{"Land_CampingTable_F", {5,0,0}, ""},
+		{"Land_ConnectorTent_01_floor_light_F", {5,0,0}, ""},
+
+		{"Land_Sign_WarningMilitaryArea_F", {5,0,0}, ""},
+		{"Land_SignM_WarningMilitaryArea_english_F", {5,0,0}, ""},
+		{"Land_Sign_WarningMilAreaSmall_F", {5,0,0}, ""},
+		{"Land_SignM_WarningMilAreaSmall_english_F", {5,0,0}, ""},
+		{"Land_Sign_WarningMilitaryVehicles_F", {5,0,0}, ""},
+		{"Land_SignM_WarningMilitaryVehicles_english_F", {5,0,0}, ""},
+		{"Land_Sign_MinesDanger_English_F", { 5,0,0}, ""},
+
+		{"Land_BarGate_01_open_F", { 10,0,0}, ""},
+		{"Land_Mil_WiredFence_Gate_F", { 10,0,0}, ""}
+	};
+
+	//If first element is array, you can overwrite default name as shown below
+	AW_bluforLogistics[] = {
+		{{"Land_Pallet_MilBoxes_F", "Supply Box"}, {200,0,200}, ""},
+		{{"B_Slingload_01_Cargo_F", "FOB Container"}, {300,500,0}, "(count AW_fobDetails) < (getNumber(missionConfigFile >> 'Core_Settings' >> 'AW_maxFobs'))"},
+		{{"B_Truck_01_box_F", "FOB Truck"}, {350,500,0}, "(count AW_fobDetails) < (getNumber(missionConfigFile >> 'Core_Settings' >> 'AW_maxFobs'))"},
+
+		{{"Land_RepairDepot_01_tan_F", "Recycle Building"}, {250,0,0}, ""},
+		{{"B_Radar_System_01_F", "Radar"}, {1000,0,0}, ""},
+		{"Land_HelipadSquare_F", {250,0,0}, ""}, //Helipads to increase rotary capacity
+		{"Land_HelipadCircle_F", {250,0,0}, ""},
+		{"Land_HelipadRescue_F", {250,0,0}, ""},
+		{"Land_TentHangar_V1_F", {500,0,0}, ""}, //Hangar to increase fixed wing capacity
+
+		{"Box_Syndicate_Ammo_F", {5,0,0}, ""},
+		
+		{"B_Truck_01_repair_F", {250,50,0}, ""},
+		{"B_Truck_01_ammo_F", {200,50,50}, ""},
+		{"B_Truck_01_fuel_F", {200,100,0}, ""},
+
+		{{"B_Truck_01_medical_F", "Mobile Respawn Vehicle"}, {200,100,0}, ""},
+		{{"B_Truck_01_medical_F", "Mobile Respawn Vehicle (free - max 1)"}, {0,0,0}, "AW_mobileRespawns isEqualTo []"},
+
+		{"B_Slingload_01_Repair_F", {600,0,0}, ""},
+		{"B_Slingload_01_Fuel_F", {100,500,0}, ""},
+		{"B_Slingload_01_Ammo_F", {100,0,500}, ""},
+
+		{{"C_IDAP_Truck_02_water_F", "Relief Truck"}, {50,50,0}, "AW_secondaryActive isEqualTo ""Relief"""},
+		{{"Land_PortableWeatherStation_01_olive_F", "Listening Post"}, {250,0,0}, "AW_secondaryActive isEqualTo ""listeningPost"" && {nearestObjects [player,[""Land_PortableWeatherStation_01_olive_F""],100] isEqualTo []}"}
+	};
+
+	//categories supported: medicalCrateCategory,lightWeaponCrateCategory,heavyWeaponCrateCategory,staticWeaponCrateCategory,miscWeaponCrateCategory
+	AW_supplyCrates[] = { //format {"category","title","box size",{resources},{contents}}
+		{"medicalCrateCategory","Small Medical Crate","medical",{25,0,0},{
+			{"ACE_packingBandage",50},
+			{"ACE_fieldDressing",50},
+			{"ACE_quikclot",50},
+			{"ACE_elasticBandage",50},
+			{"ACE_tourniquet",20},
+			{"ACE_morphine",20},
+			{"ACE_epinephrine",10},
+			{"ACE_splint",20},
+			{"ACE_salineIV",5},
+			{"ACE_salineIV_500",8},
+			{"ACE_salineIV_250",10},
+			{"TWC_Item_Medical_SutureKit_25",5}
+		}},
+		{"medicalCrateCategory","Large Medical Crate","medical",{75,0,0},{
+			{"ACE_packingBandage",100},
+			{"ACE_fieldDressing",100},
+			{"ACE_quikclot",100},
+			{"ACE_elasticBandage",100},
+			{"ACE_tourniquet",40},
+			{"ACE_morphine",40},
+			{"ACE_epinephrine",20},
+			{"ACE_salineIV",10},
+			{"ACE_salineIV_500",16},
+			{"ACE_salineIV_250",20},
+			{"TWC_Item_Medical_SutureKit_25",10}
+		}},
+		{"staticWeaponCrateCategory","Mk6 Mortar","large",{50,0,0},{
+			{"ACE_EarPlugs",2},
+			{"ACE_MapTools",2},
+			{"ACE_Vector",2},
+			{"ACE_artilleryTable",2},
+			{"B_Mortar_01_weapon_F",2},
+			{"B_Mortar_01_support_F",2}
+		}},
+		{"staticWeaponCrateCategory","Mk30 HMG","large",{50,0,0},{
+			{"ACE_EarPlugs",2},
+			{"B_HMG_01_weapon_F",1},
+			{"B_HMG_01_high_weapon_F",1},
+			{"B_HMG_01_support_F",1},
+			{"B_HMG_01_support_high_F",1}
+		}},
+		{"staticWeaponCrateCategory","Mk32 GMG","large",{50,0,0},{
+			{"ACE_EarPlugs",2},
+			{"B_GMG_01_high_weapon_F",1},
+			{"B_GMG_01_weapon_F",1},
+			{"B_HMG_01_support_F",1},
+			{"B_HMG_01_support_high_F",1}
+		}},
+		{"lightWeaponCrateCategory","Gruppe Resupply","medium",{50,0,0},{
+			{"30Rnd_65x39_caseless_mag",25},
+			{"30Rnd_556x45_Stanag_Sand",25},
+			{"20Rnd_762x51_Mag",20},
+			{"100Rnd_65x39_caseless_mag",10},
+			{"150Rnd_556x45_Drum_Mag_F",8},
+						
+			{"HandGrenade",6},
+			{"MiniGrenade",6},
+			{"SmokeShell",4},
+			{"SmokeShellBlue",4},
+			{"SmokeShellOrange",4},
+			{"1Rnd_HE_Grenade_shell",10},
+			{"UGL_FlareGreen_F",10},
+			{"UGL_FlareYellow_F",10},
+			{"1Rnd_Smoke_Grenade_shell",5},
+			{"1Rnd_SmokeRed_Grenade_shell",5}
+		}},
+		{"miscWeaponCrateCategory","Explosives","small",{75,0,0},{
+			{"DemoCharge_Remote_Mag",6},
+			{"ACE_Clacker",2},
+			{"ACE_DefusalKit",2},
+			{"MineDetector",2}
+		}},
+		{"miscWeaponCrateCategory","Empty Crate (Small)","small",{5,0,0},{
+
+		}},
+		{"miscWeaponCrateCategory","Empty Crate (Medium)","medium",{5,0,0},{
+			
+		}}
+	};
+};
---
+++ Frontlines.ruha/Config/Config_Civilian_European.hpp
@@ -0,0 +1,59 @@
+/*
+	Faction: Livonian
+	Author: Stanhope
+	Requires: Contact (users do not require it to play)
+*/
+class Civilian_Setup {
+	AW_civilianTypes[] = {
+		"C_Man_1_enoch_F",
+		"C_Man_2_enoch_F",
+		"C_Man_3_enoch_F",
+		"C_Man_4_enoch_F",
+		"C_Man_5_enoch_F",
+		"C_Man_6_enoch_F",
+		"C_man_p_beggar_F_euro",
+		"C_Man_casual_1_F_euro",
+		"C_Man_casual_2_F_euro",
+		"C_Man_casual_3_F_euro",
+		"C_Man_casual_4_v2_F_euro",
+		"C_Man_casual_5_v2_F_euro",
+		"C_Man_casual_6_v2_F_euro",
+		"C_Man_casual_7_F_euro",
+		"C_Man_casual_8_F_euro",
+		"C_Man_casual_9_F_euro",
+		"C_Man_casual_4_F_euro",
+		"C_Man_casual_5_F_euro",
+		"C_Man_casual_6_F_euro",
+		"C_man_polo_1_F_euro",
+		"C_man_polo_2_F_euro",
+		"C_man_polo_3_F_euro",
+		"C_man_polo_4_F_euro",
+		"C_man_polo_5_F_euro",
+		"C_man_polo_6_F_euro",
+		"C_man_shorts_1_F_euro",
+		"C_man_p_fugitive_F_euro",
+		"C_man_p_shorts_1_F_euro",
+		"C_Farmer_01_enoch_F"
+	};
+
+	AW_civilianVehicleTypes[] = {
+		"C_Truck_02_fuel_F",
+		"C_Truck_02_transport_F",
+		"C_Truck_02_covered_F",
+		"C_Truck_02_box_F",
+		"C_Offroad_01_F",
+		"C_Offroad_01_covered_F",
+		"C_Tractor_01_F"
+	};
+
+	AW_civilianAircraftTypes[] = {
+		"C_Plane_Civil_01_F",
+		"C_Plane_Civil_01_racing_F",
+		"C_Heli_Light_01_civil_F",
+		"C_Heli_light_01_blue_F",
+		"C_Heli_light_01_red_F",
+		"C_Heli_light_01_ion_F",
+		"C_Heli_light_01_blueLine_F"
+	};
+};
+
